cebbb73c0bab24092a3179a173b79452657d664e
daibogh <DaiBogh@gmail.com>
2021-07-20 22:26:22 +0300

    feat: added button example

--- src/components/Button/Button.md
+++ src/components/Button/Button.md
@@ -1,4 +1,16 @@
-Button example:
+Button example 1:
+```tsx
+import {ButtonBorderRadius, ButtonBorderColor, ButtonColor, ButtonBackgroundColor} from './Button';
+;<Button text="hi!" borderColor={ButtonBorderColor.white} borderRadius={ButtonBorderRadius.circle} color={ButtonColor.white} backgroundColor={ButtonBackgroundColor.red}/>
 ```
-<Button text="🍕" />
+Button example 2:
+```tsx
+import {ButtonBorderRadius, ButtonBorderColor, ButtonColor, ButtonBackgroundColor} from './Button';
+;<Button text="hi!" borderColor={ButtonBorderColor.white} borderRadius={ButtonBorderRadius.circle} color={ButtonColor.white} backgroundColor={ButtonBackgroundColor.blue}/>
 ```
+Button example 3:
+```tsx
+import {ButtonBorderRadius, ButtonBorderColor, ButtonColor, ButtonBackgroundColor} from './Button';
+;<Button text="🍕" borderColor={ButtonBorderColor.white} borderRadius={ButtonBorderRadius.circle} color={ButtonColor.white} backgroundColor={ButtonBackgroundColor.transparent}/>
+```
+
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,33 @@
 import React from "react";
-
+import styled from 'styled-components/macro'
+export enum ButtonBorderRadius {
+    circle = '100%'
+}
+export enum ButtonBorderColor {
+    white = 'white'
+}
+export enum ButtonColor {
+    white = 'white'
+}
+export enum ButtonBackgroundColor {
+    red = '#F21F5B',
+    blue = '#3951E7',
+    transparent = 'transparent'
+}
 type ButtonProps = {
     text?: string
+    borderRadius?: ButtonBorderRadius
+    borderColor?: ButtonBorderColor
+    color?: ButtonColor
+    backgroundColor?: ButtonBackgroundColor
 }
-const Button: React.FC<ButtonProps> = ({text}) => {
-    return <button type='button'>{text}</button>
+const _Button = styled.button<ButtonProps>`
+    color: ${({color}) => color};
+    border-radius: ${({borderRadius}) => borderRadius};
+    border: ${({borderColor}) => `1px solid ${borderColor}`};
+    background-color: ${({backgroundColor}) => backgroundColor};
+`
+const Button: React.FC<ButtonProps> = ({text, ...props}) => {
+    return <_Button {...props} type='button'>{text}</_Button>
 }
 export default Button
